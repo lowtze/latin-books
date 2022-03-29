8248fe8f9cde200c8664d8e61b32e42760edb014
lowtze <17298933+lowtze@users.noreply.github.com>
2022-03-29 10:50:00 -0400

    added new script for quote formatting

---
+++ scripts/formatQuotes.py
@@ -0,0 +1,10 @@
+filename = 'test.txt'
+
+with open(filename, 'r+') as f:
+        lines=f.readlines()
+        for i, line in enumerate(lines):
+                lines[i] = '>*' + lines[i].strip() + '*\n\n' #two new lines to leave a space between each
+        f.seek(0) # go back to start
+        for line in lines:
+                f.write(line) #write the modified lines
+        
