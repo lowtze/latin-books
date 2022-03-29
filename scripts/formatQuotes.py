729bbf2c97c64af31b81207ae1040cf55a58f0a7
lowtze <17298933+lowtze@users.noreply.github.com>
2022-03-29 10:56:40 -0400

    fixed errors

--- scripts/formatQuotes.py
+++ scripts/formatQuotes.py
@@ -1,9 +1,9 @@
 filename = 'test.txt'
 
-with open(filename, 'r+') as f:
+with open(filename, 'r+', encoding="utf8") as f:
         lines=f.readlines()
         for i, line in enumerate(lines):
-                lines[i] = '>*' + lines[i].strip() + '*\n\n' #two new lines to leave a space between each
+                lines[i] = '>*' + lines[i].strip() + '*\n>\n' #two new lines to leave a space between each
         f.seek(0) # go back to start
         for line in lines:
                 f.write(line) #write the modified lines
